67a40b09fcebb999adc9f8ac939a118626cb00c7
christine09 <chickchick1044@naver.com>
2019-01-23 18:20:09 +0900

    로그인 액티비티 추가

--- app/src/main/java/com/example/kimjihyeon/myapplication/MainActivity.java
+++ app/src/main/java/com/example/kimjihyeon/myapplication/MainActivity.java
@@ -2,20 +2,25 @@ package com.example.kimjihyeon.myapplication;
 
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private FloatingActionButton fab1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab1 = (FloatingActionButton)findViewById(R.id.fab1);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -46,7 +51,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        fab1.setOnClickListener(this);
 
     }//onCreate ended
 
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id){
+            case R.id.fab1:
+                //floating action btn 클릭시
+                break;
+        }
+
+    }
 }//mainActivity ended
